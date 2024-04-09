72714c1290bbb5108d5732d1d1ca534e4414e82b
dundunkirk <78774917@qq.com>
2024-04-09 09:52:34 +0800

    update

--- exercises/variables/variables5.rs
+++ exercises/variables/variables5.rs
@@ -3,11 +3,11 @@
 // Execute `rustlings hint variables5` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
+// shadowing 技术可以让我们在同一个作用域中多次使用同一个变量名，而不会导致冲突。
 
 fn main() {
     let number = "T-H-R-E-E"; // don't change this line
     println!("Spell a Number : {}", number);
-    number = 3; // don't rename this variable
+    let number = 3; // don't rename this variable
     println!("Number plus two is : {}", number + 2);
 }
--- exercises/variables/variables6.rs
+++ exercises/variables/variables6.rs
@@ -3,9 +3,9 @@
 // Execute `rustlings hint variables6` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
+// 在 Rust 中，常量必须有显式类型
 
-const NUMBER = 3;
+const NUMBER: i32 = 3;
 fn main() {
     println!("Number {}", NUMBER);
 }
